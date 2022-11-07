5d1b95935813962074ab820ac98ccf61214ebcae
gaurav-b <sunymca619@gmail.com>
2022-11-08 02:05:54 +0530

    making use or JPA repository with H2 DB

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,24 +2,34 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
 
-	@JsonIgnore
+//	@JsonIgnore
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2, message = "Name should have atleast 2 characters")
-	@JsonProperty("user_name")
+//	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "Birth date should be in the past")
-	@JsonProperty("birth_date")
+//	@JsonProperty("birth_date")
 	private LocalDate birthDate;
+	
+	protected User() {
+		
+	}
 	
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJpaResource.java
@@ -0,0 +1,71 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.in28minutes.rest.webservices.restfulwebservices.jpa.UserRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class UserJpaResource {
+
+	private UserRepository repository;
+	
+	public UserJpaResource(UserRepository repository) {
+		this.repository=repository;
+	}
+	
+	@GetMapping(path = "/jpa/users")
+	public List<User> retrieveAllUsers() {
+		return repository.findAll();
+	}
+	
+	@GetMapping("/jpa/users/{id}")
+	public EntityModel<User> retriveUser(@PathVariable Integer id) {
+		Optional<User> user = repository.findById(id);
+		
+		if (user.isEmpty()) {
+			throw new UserNotFoundException("id:" + id);
+		}
+		
+		EntityModel<User> entityModel = EntityModel.of(user.get());
+		
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		return entityModel;
+	}
+	
+	@DeleteMapping("/jpa/users/{id}")
+	public void deleteUser(@PathVariable Integer id) {
+		repository.deleteById(id);
+	}
+	
+	@PostMapping("/jpa/users")
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+		User savedUser = repository.save(user);
+		
+		// location below is holding the path of the new user created
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest() // will retrun the path of the current request
+				.path("/{id}") // will append a placeholder to the request
+				.buildAndExpand(savedUser.getId()) // will replace the placeholder with this value
+				.toUri();
+		
+		return ResponseEntity.created(location).build();
+	}
+}
