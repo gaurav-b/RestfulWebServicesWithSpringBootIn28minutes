4e1426f327ef686e40745a9c0fb0a11581b1f852
gaurav-b <sunymca619@gmail.com>
2022-11-08 17:06:56 +0530

    adding spring security

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.in28minutes.rest.webservices.restfulwebservices.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		// All requests must be authenticated
+		http.authorizeHttpRequests(
+			auth -> auth.anyRequest().authenticated()
+		);
+		
+		// if a request is not authenticated, a web page is shown
+		// actually it will show a alert box kind of popup asking creds
+		http.httpBasic(withDefaults());
+		
+		// disable csrf
+		http.csrf().disable();
+		
+		return http.build();
+	}
+}
